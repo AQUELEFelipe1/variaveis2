e06c9a2982bd925368f2bbfe59118fa6abe6499e
AQUELEFelipe1 <127846083+AQUELEFelipe1@users.noreply.github.com>
2024-08-29 01:30:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let anoAtual = 2024;
+const nomeCompleto = "Felipe Medeiros Sousa";
+let idade = 17;
+const cursoFaculdade = "alguma coisa";
+let cidadeAtual = "SJP";
+
+console.log(`Ola eu sou o ${nomeCompleto} anos, moro em ${cidadeAtual}quero ano que vem estar cursando ${cursoFaculdade}`);
+
+anoAtual = 2025;
+idade = 18;
+cidadeAtual = "SJP";
+depoisMarço = "exercito";
+lazer = "valorant e genshin"
+
+console.log(`Bom ano que vem terei ${idade} e continuarei morando em ${cidadeAtual} e provavelmente estarei no ${depoisMarço} vou ganhar dinheiro, porém irei ter cada vez menos tempo para jogar ${lazer} com meus amigos`);
